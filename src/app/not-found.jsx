eb6c14b92335e3f10db53ceaf1451db10ccc5c63
khirul-alam <yeasin1983@gmail.com>
2026-07-06 11:16:15 +0600

    Not found page update

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,16 +1,18 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { TbError404 } from "react-icons/tb";
 
 export default function NotFound() {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-4 text-center">
-      <Image
+      {/* <Image
         src="/public/images/404-illustration.jpg"
         alt="Page not found"
         width={288}
         height={288}
         className="w-72"
-      />
+      /> */}
+      <TbError404 />
       <h1 className="text-3xl font-bold">Oops! Page Not Found</h1>
       <p className="text-gray-500">
         The page you are looking for doesn&apos;t exist or has been moved.
